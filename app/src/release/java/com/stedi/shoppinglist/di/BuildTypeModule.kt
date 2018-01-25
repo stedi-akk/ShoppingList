59175863b151d4624ba0aedea9ef6f3e361dfaf2
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2018-01-25 20:47:01 +0100

    repository as singleton

--- app/src/release/java/com/stedi/shoppinglist/di/BuildTypeModule.kt
+++ app/src/release/java/com/stedi/shoppinglist/di/BuildTypeModule.kt
@@ -5,10 +5,12 @@ import com.stedi.shoppinglist.model.repository.DatabaseShoppingRepository
 import com.stedi.shoppinglist.model.repository.ShoppingRepository
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class BuildTypeModule {
     @Provides
+    @Singleton
     fun provideShoppingRepository(@AppContext context: Context): ShoppingRepository {
         return DatabaseShoppingRepository(context, "shopping_database", 1)
     }
--- app/src/slowdebug/java/com/stedi/shoppinglist/di/BuildTypeModule.kt
+++ app/src/slowdebug/java/com/stedi/shoppinglist/di/BuildTypeModule.kt
@@ -6,10 +6,12 @@ import com.stedi.shoppinglist.model.repository.ShoppingRepository
 import com.stedi.shoppinglist.model.repository.SlowShoppingRepository
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class BuildTypeModule {
     @Provides
+    @Singleton
     fun provideShoppingRepository(@AppContext context: Context): ShoppingRepository {
         return SlowShoppingRepository(DatabaseShoppingRepository(context, "shopping_database", 1))
     }
